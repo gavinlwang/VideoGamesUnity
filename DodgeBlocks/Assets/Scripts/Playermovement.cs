71485ca79b5880d5bfc6674132573331b1e9205c
gavinlwang <gavini.l.wang@outlook.com>
2021-11-12 23:36:36 -0500

    Signed-off-by: gavinlwang <gavini.l.wang@outlook.com>

---
+++ DodgeBlocks/Assets/Scripts/Playermovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    bool force = false;
+    public float forwardForce = 400f;
+    public float sidewaysForce = 60f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("space"))
+        {
+            force = true;
+        }
+
+        if (force == true)
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+            if (Input.GetKey("d"))
+            {
+                rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            }
+
+            if (Input.GetKey("a"))
+            {
+                rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            }
+        }
+
+        if (rb.position.y < -2f)
+        {
+            Invoke("freeze", 1f);
+        }
+
+        if (rb.position.x > 7.5)
+        {
+            Invoke("freeze", 1f);
+        }
+
+        if (rb.position.x < -7.5)
+        {
+            Invoke("freeze", 1f);
+        }
+    }
+
+    void freeze ()
+    {
+        rb.constraints = RigidbodyConstraints.FreezePosition;
+        FindObjectOfType<GameManager>().EndGame();
+    }
+
+}
---
+++ DodgeBlocks/Assets/Scripts/Score.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public Text score;
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = player.position.z.ToString("0");
+    }
+}
